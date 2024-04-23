fe2b3fac9ac150213d08c474b06dfbb714583684
Tesnim19 <tesnimabdi67@gmail.com>
2024-04-23 04:17:17 +0300

    HomePage creation

---
+++ backend/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import sequelize from './utils/database';
+import File from './models/FileModel';
+import fileRoutes from './routes/File.routes'
+const cors = require('cors');
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use('/api', fileRoutes);
+
+
+
+sequelize.sync({ force: true }).then(() => {
+  console.log('Database synchronized successfully.');
+}).catch(error => {
+  console.error('Error synchronizing database:', error);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Multi-File Uploader App</h1>
+      <p>This is the homepage of the application.</p>
+      <p>You can navigate to other pages using the links in the navigation bar.</p>
+    </div>
+  );
+};
+
+export default HomePage;
